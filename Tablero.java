1e589eb03afb42b17c9d9a7adf103ee822747169
Josu <jalvarezfer@gmail.com>
2016-02-22 22:28:50 +0100

    terminado Tablero
    se ha terminado la implementacion del metodo crearTablero()

--- Tablero.java
+++ Tablero.java
@@ -21,11 +21,11 @@ public class Tablero {
         return miTablero;
     }
 
-    private void anadirCasillaTablero(Casilla pCasilla){
+    private void anadirCasillaTablero(Casilla pCasilla) {
         this.tablero[pCasilla.getPosX()][pCasilla.getPosY()] = pCasilla;
     }
 
-    public void crearTablero(int nivel){
+    public void crearTablero(int nivel) {
         if (nivel == 1) {
             this.x = 7;
             this.y = 10;
@@ -46,68 +46,69 @@ public class Tablero {
             }
         }*/
 
-        for (int i=1; i<= numMinas; i++){ //primero generamos las minas de forma aleatoria en el mapa
+        for (int i = 1; i <= numMinas; i++) { //primero generamos las minas de forma aleatoria en el mapa
             Random rand = new Random();
-            int auxX= rand.nextInt(this.x-1); //la clase random devuelve un numero entre 0 y el numero
-            int auxY= rand.nextInt(this.y-1); // indicado por el usuario
-            this.anadirCasillaTablero(new CasillaMina(auxX,auxY));
-            if (auxX != 0){
-                if (auxY != 0){
-                    if (this.tablero[auxX-1][auxY-1] instanceof CasillaVacia){               //miramos la casilla superior izq
-                        this.anadirCasillaTablero(new CasillaNumerica(auxX-1,auxY-1));
-                    }
-                    else if (this.tablero[auxX-1][auxY-1] instanceof  CasillaNumerica){     //mmiramos la casilla superior izq
-                        this.tablero[auxX-1][auxY-1].aumentarValor();
-                    }
+            int auxX = rand.nextInt(this.x - 1); //la clase random devuelve un numero entre 0 y el numero
+            int auxY = rand.nextInt(this.y - 1); // indicado por el usuario
 
-                    if(this.tablero[auxX][auxY-1] instanceof CasillaVacia){  //miramos la casilla superior
-                        this.anadirCasillaTablero(new CasillaNumerica(auxX,auxY-1));
-                    }
-                    else if(this.tablero[auxX][auxY-1] instanceof CasillaNumerica){  //miramos la casilla superior
-                        this.tablero[auxX][auxY-1].aumentarValor();
-                    }
+            this.anadirCasillaTablero(new CasillaMina(auxX, auxY));
 
-                    if (auxX != this.x-1){
-
-                    }
+            if (auxY != 0 && auxX != 0) { //miramos la casilla superior izq
+                if (this.tablero[auxX - 1][auxY - 1] instanceof CasillaVacia) {
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX - 1, auxY - 1));
+                } else if (this.tablero[auxX - 1][auxY - 1] instanceof CasillaNumerica) {
+                    this.tablero[auxX - 1][auxY - 1].aumentarValor();
+                }
+            }
+            if (auxY != 0) {//miramos la casilla superior
+                if (this.tablero[auxX][auxY - 1] instanceof CasillaVacia) {
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX, auxY - 1));
+                } else if (this.tablero[auxX][auxY - 1] instanceof CasillaNumerica) {
+                    this.tablero[auxX][auxY - 1].aumentarValor();
+                }
+            }
+            if (auxY != 0 && auxX != this.x - 1) {
+                if (this.tablero[auxX + 1][auxY - 1] instanceof CasillaVacia) { //miramos la casilla superior derecha
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX + 1, auxY - 1));
+                } else if (this.tablero[auxX + 1][auxY - 1] instanceof CasillaNumerica) {
+                    this.tablero[auxX - 1][auxY - 1].aumentarValor();
+                }
+            }
+            if (auxX != this.x - 1) {
+                if (this.tablero[auxX + 1][auxY] instanceof CasillaVacia) { //miramos la casilla derecha
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX + 1, auxY));
+                } else if (this.tablero[auxX + 1][auxY] instanceof CasillaNumerica) {
+                    this.tablero[auxX + 1][auxY].aumentarValor();
+                }
+            }
+            if (auxY != this.y - 1 && auxX != this.x - 1) { //miramos la casilla inferior derecha
+                if (this.tablero[auxX + 1][auxY + 1] instanceof CasillaVacia) {
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX + 1, auxY + 1));
+                } else if (this.tablero[auxX + 1][auxY + 1] instanceof CasillaNumerica) {
+                    this.tablero[auxX + 1][auxY + 1].aumentarValor();
+                }
+            }
+            if (auxY != this.y - 1) { //comprobamos la casilla de abajo
+                if (this.tablero[auxX][auxY + 1] instanceof CasillaVacia) {
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX, auxY + 1));
+                } else if (this.tablero[auxX][auxY + 1] instanceof CasillaNumerica) {
+                    this.tablero[auxX][auxY + 1].aumentarValor();
+                }
+            }
+            if (auxY != this.y - 1 && auxX != 0) {//comprobamos la casilla de abajo a la izq
+                if (this.tablero[auxX - 1][auxY + 1] instanceof CasillaVacia) {
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX - 1, auxY + 1));
+                } else if (this.tablero[auxX - 1][auxY + 1] instanceof CasillaNumerica) {
+                    this.tablero[auxX - 1][auxY + 1].aumentarValor();
+                }
+            }
+            if (auxX != 0) {//comprobamos la casilla de la izq
+                if (this.tablero[auxX - 1][auxY] instanceof CasillaVacia) {
+                    this.anadirCasillaTablero(new CasillaNumerica(auxX - 1, auxY));
+                } else if (this.tablero[auxX - 1][auxY] instanceof CasillaNumerica) {
+                    this.tablero[auxX - 1][auxY].aumentarValor();
                 }
             }
         }
-
-        for (int i=0; i<x;i++) { //rellenamos todas las casillas con minas
-            for (int j=0;j<y;j++){
-                int auxContMinas = 0;
-                if (this.tablero[i][j] instanceof CasillaVacia){
-                    if (i != 0){
-                        if (this.tablero[i-1][j] instanceof CasillaMina) {
-                            auxContMinas++;
-                        }
-                        if (j != 0){
-                            if (this.tablero[i][j-1] instanceof  CasillaMina){
-                                auxContMinas++;
-                            }
-                        }
-                    }
-                    if (i != x-1){
-                        if (this.tablero[i+1][j] instanceof CasillaMina) {
-                            auxContMinas++;
-                        }
-                        if (j !)
-                    }
-            }
-        }
-
-
     }
-
-
-
-
-
-
-
-}
-
-
-
 }
