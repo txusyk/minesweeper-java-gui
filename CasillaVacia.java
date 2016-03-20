ab5619e457d30539fa161f11560100d7ecaf9552
txusyk <jalvarezfer@gmail.com>
2016-03-21 00:29:56 +0100

    Primer Sprint (Añadidos Test)

--- CasillaVacia.java
+++ CasillaVacia.java
@@ -13,7 +13,7 @@ public class CasillaVacia extends Casilla {
 
     /**
      * @param posX
-     * @param posY En caso de clickar sobre esta casilla no  ocurrirá nada
+     * @param posY En caso de clickar sobre esta casilla no  ocurrirá nada. Solo se descubrira una casilla vacia.
      */
     public void descubrirCasilla(int posX, int posY) {
         Casilla auxCasilla = null;
---
+++ test/CasillaVaciaTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by josu on 3/21/16.
+ */
+public class CasillaVaciaTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDescubrirCasilla() throws Exception {
+
+    }
+}
