5b7426117d7ebca114cddc8b54c40701fd822f0b
txusyk <jalvarezfer@gmail.com>
2016-03-27 05:02:35 +0200

    Añadida la clase Jugador (error en el anterior commit)

---
+++ Jugador.java
@@ -0,0 +1,14 @@
+/**
+ * Created by david on 26/03/2016.
+ */
+public class Jugador {
+
+    private String nombre;
+
+    public Jugador(String pNombre) {
+        this.nombre = pNombre;
+    }
+
+
+}
+
